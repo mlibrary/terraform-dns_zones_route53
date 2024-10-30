28702f74c63f121252bb06346640dbebea2ada6f
Ryan Rotter <rrotter@users.noreply.github.com>
2024-10-30 12:58:16 -0400

    add *_ttl variables
    Allows setting TTL for every record of a type for the whole module.

--- variables.tf
+++ variables.tf
@@ -50,3 +50,33 @@ variable "allow_overwrite" {
   type        = bool
   default     = false
 }
+
+variable "a_ttl" {
+  description = "TTL for A records"
+  type        = number
+  default     = 300 # default TTL from AWS console
+}
+
+variable "mx_ttl" {
+  description = "TTL for MX records"
+  type        = number
+  default     = 86400 # default TTL from AWS console
+}
+
+variable "txt_ttl" {
+  description = "TTL for TXT records"
+  type        = number
+  default     = 300 # default TTL from AWS console
+}
+
+variable "cname_ttl" {
+  description = "TTL for CNAME records"
+  type        = number
+  default     = 300 # default TTL from AWS console
+}
+
+variable "ns_ttl" {
+  description = "TTL for NS records, does not apply to delegation set"
+  type        = number
+  default     = 300 # very short, only likely to be used while moving between DNS providers
+}
